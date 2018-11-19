4c98d0865231bc0cbdd6605cb1c8b5f3ea39854c
youngho jeong <hooo5513@naver.com>
2018-11-20 01:08:31 +0900

    라우팅 구성

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/all',
+    pathMatch: 'full',
+  },
+  {
+    path: 'all',
+    component: ListComponent,
+    data: {
+      tabType: 'all',
+    },
+  },
+  {
+    path: 'active',
+    component: ListComponent,
+    data: {
+      tabType: 'active',
+    },
+  },
+  {
+    path: 'completed',
+    component: ListComponent,
+    data: {
+      tabType: 'completed',
+    },
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
